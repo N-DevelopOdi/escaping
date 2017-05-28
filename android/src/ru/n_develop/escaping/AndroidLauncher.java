fb09cc8c37da127b414e0fe058adb4b1be1320e3
DIMA_N <dim9044@yandex.ru>
2017-05-28 14:22:35 +0300

    Первый комит

---
+++ android/src/ru/n_develop/escaping/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.n_develop.escaping;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.n_develop.escaping.MainEscaping;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainEscaping(), config);
+	}
+}
---
+++ desktop/src/ru/n_develop/escaping/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.n_develop.escaping.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.n_develop.escaping.MainEscaping;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainEscaping(), config);
+	}
+}
